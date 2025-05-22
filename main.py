ad0009b272c073f376dd25148dcea34cc587b5e6
saygintugce <tugcesaygin.02@gmail.com>
2025-05-22 15:54:23 +0300

    column name changes

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("deneme")
+import pandas as pd
+import seaborn as sns
+
+
+df =  pd.read_csv('survey-lung-cancer.csv')
+df.columns
+
+[column + "_FLAG" if df[column].dtypes != "O" else column for column in df.columns]
+
+[column + "_FLAG" if df[column].dtypes == "O" else 'NO_' + column if df[column].dtype == 'int64' else column for column in df.columns]
+
